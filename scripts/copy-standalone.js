6954584c5a50ff17f8946e8c785cd0ed79d6d3f5
nateelive <natee.live@gmail.com>
2026-06-08 14:54:39 +0700

    fixed bug food-ordering-system

---
+++ scripts/copy-standalone.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyDir(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+  for (const item of fs.readdirSync(src)) {
+    const srcPath = path.join(src, item);
+    const destPath = path.join(dest, item);
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+copyDir('.next/static', '.next/standalone/.next/static');
+
+if (fs.existsSync('public')) {
+  copyDir('public', '.next/standalone/public');
+}
+
+console.log('Standalone files copied successfully.');
